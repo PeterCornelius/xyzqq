48fd229b4846ed79e0b14a486e188679f3537a57
petercornelius <peter.cornelius@gmail.com>
2021-09-27 14:24:05 +0000

    Add formatting

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! Your function executed successfully! Hello World',
+        message: 'Go Serverless v2.0! <br/>Your function executed successfully! <br/><b>Hello World</b>',
 	input: event,
       },
       null,
